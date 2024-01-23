50ecf4db1738e1440018a8035c70a8bc65bc2101
Aaron Thapa <notsmart98@gmail.com>
2024-01-23 14:10:04 +0545

    more updates

--- py/main.py
+++ py/main.py
@@ -1,8 +1,15 @@
 import asyncio
+import dataclasses
 import json
 import numpy as np
 import pygame
 import websockets
+
+@dataclasses.dataclass
+class LED:
+    r: int
+    g: int
+    b: int
 
 async def main():
     pygame.init()
@@ -13,7 +20,13 @@ async def main():
             await socket.send(b'{"id":0,"module":"drs","function":"tapeled_get","params":[]}')
             message = await socket.recv()
             rgb_values = [x & 0xFE for x in json.loads(message[:-1])["data"][0]]
-            image = np.array(rgb_values, dtype=np.uint8).reshape(49, 38, 3).transpose(1, 0, 2)
+            values = np.array(rgb_values, dtype=np.uint8).reshape(49, 38, 3)
+            image = values.transpose(1, 0, 2)
+            leds = np.array([LED(*x) for x in values.reshape(49*38, 3)]).reshape(49, 38)
+            for y in range(49):
+                for x in range(38):
+                    # TODO: write the thing to dump the thing
+                    pass
             surface = pygame.surfarray.make_surface(image)
             display.blit(pygame.transform.scale(surface, (38*10, 49*10)), (0, 0))
             pygame.display.update()
