dd01fb3a5f694bde09233e60f8bd4a161eba4fcc
Aaron Thapa <notsmart98@gmail.com>
2024-01-24 15:58:44 +0545

    add performance test

---
+++ py/test.py
@@ -0,0 +1,11 @@
+import serial
+from time import perf_counter
+
+with serial.Serial("COM5", 921600) as ser:
+    while True:
+        t1_start = perf_counter()
+        ser.write(b' ' * 49 * 38 * 3)
+        if ser.read(1) != b'a':
+            break
+        t1_stop = perf_counter()
+        print(f'Elapsed time: {t1_stop - t1_start:.3f} s Framerate {1 / (t1_stop - t1_start):.3f}')
