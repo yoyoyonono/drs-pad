4508429625dfc3916855f7c27d06b26ae99d4cc3
Aaron Thapa <notsmart98@gmail.com>
2024-01-24 14:59:57 +0545

    try serial

--- pico/main.cpp
+++ pico/main.cpp
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include "tusb.h"
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 #include "pico/multicore.h"
@@ -10,6 +12,12 @@
 LED led_buffer_1[NUM_LEDS];
 LED led_buffer_2[NUM_LEDS];
 
+volatile bool usb_transfer_finished = false;
+volatile bool dma_transfer_finished = false;
+
+volatile bool usb_buffer = false; // false = buffer 1, true = buffer 2
+volatile bool dma_buffer = true; // false = buffer 1, true = buffer 2
+
 // general loop
 // fill one buffer with data from usb on core 0
 // dma transfer other to leds on core 1
@@ -19,6 +27,10 @@ void core1_main();
 
 int main() {
     stdio_init_all();
+    while (!tud_cdc_connected()) {
+        tight_loop_contents();
+    }
+    printf("init\n");
     if (cyw43_arch_init()) {
         printf("Wi-Fi init failed");
         return -1;
@@ -31,13 +43,28 @@ int main() {
     }
     // start core 1
     multicore_launch_core1(core1_main);
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false); 
+
     while (true) {
-        
+        printf("led finished\n");
+        LED *led_buffer = usb_buffer ? led_buffer_2 : led_buffer_1;
+        for (int i = 0; i < NUM_LEDS; i++) {
+//            led_buffer[i] = {(uint8_t)getchar(), (uint8_t)getchar(), (uint8_t)getchar()};
+        }
+        usb_buffer = !usb_buffer;
+        usb_transfer_finished = true;
+        while (!dma_transfer_finished) {
+            tight_loop_contents();
+        }
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, usb_buffer);
+        dma_transfer_finished = false;
+        dma_buffer = !dma_buffer;
     }
 }
 
 void core1_main() {
     while (true) {
+        sleep_ms(100);
+        dma_transfer_finished = true;
     }
 }
