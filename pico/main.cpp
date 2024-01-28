3f5826d8be0e7fe2cae27b96bb0c69805fd0029e
Aaron Thapa <notsmart98@gmail.com>
2024-01-28 15:35:24 +0545

    remove 2-8

--- pico/main.cpp
+++ pico/main.cpp
@@ -21,13 +21,13 @@ LED led_buffer_1[NUM_LEDS];
 LED led_buffer_2[NUM_LEDS];
 
 Adafruit_NeoPixel strip_1 = Adafruit_NeoPixel(LED_STRIP_LENGTH * 5, LED_STRIP_1_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_2 = Adafruit_NeoPixel(LED_STRIP_LENGTH * 5, LED_STRIP_2_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_3 = Adafruit_NeoPixel(LED_STRIP_LENGTH * 5, LED_STRIP_3_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_4 = Adafruit_NeoPixel(LED_STRIP_LENGTH * 5, LED_STRIP_4_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_5 = Adafruit_NeoPixel(LED_STRIP_LENGTH * 5, LED_STRIP_5_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_6 = Adafruit_NeoPixel(LED_STRIP_LENGTH * 5, LED_STRIP_6_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_7 = Adafruit_NeoPixel(LED_STRIP_LENGTH * 4, LED_STRIP_7_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_8 = Adafruit_NeoPixel(LED_STRIP_LENGTH * 4, LED_STRIP_8_PIN, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel strip_2 = Adafruit_NeoPixel(LED_STRIP_LENGTH * 5, LED_STRIP_2_PIN, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel strip_3 = Adafruit_NeoPixel(LED_STRIP_LENGTH * 5, LED_STRIP_3_PIN, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel strip_4 = Adafruit_NeoPixel(LED_STRIP_LENGTH * 5, LED_STRIP_4_PIN, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel strip_5 = Adafruit_NeoPixel(LED_STRIP_LENGTH * 5, LED_STRIP_5_PIN, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel strip_6 = Adafruit_NeoPixel(LED_STRIP_LENGTH * 5, LED_STRIP_6_PIN, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel strip_7 = Adafruit_NeoPixel(LED_STRIP_LENGTH * 4, LED_STRIP_7_PIN, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel strip_8 = Adafruit_NeoPixel(LED_STRIP_LENGTH * 4, LED_STRIP_8_PIN, NEO_GRB + NEO_KHZ800);
 
 volatile bool usb_transfer_finished = false;
 volatile bool dma_transfer_finished = false;
@@ -85,35 +85,35 @@ void core1_main() {
         for (int i = 0; i < LED_STRIP_LENGTH * 5; i++) {
             strip_1.setPixelColor(i, strip_1.Color(led_buffer[i].r, led_buffer[i].g, led_buffer[i].b));
         }
-        for (int i = LED_STRIP_LENGTH * 5; i < LED_STRIP_LENGTH * 10; i++) {
-            strip_2.setPixelColor(i, strip_2.Color(led_buffer[i].r, led_buffer[i].g, led_buffer[i].b));
-        }
-        for (int i = LED_STRIP_LENGTH * 10; i < LED_STRIP_LENGTH * 15; i++) {
-            strip_3.setPixelColor(i, strip_3.Color(led_buffer[i].r, led_buffer[i].g, led_buffer[i].b));
-        }
-        for (int i = LED_STRIP_LENGTH * 15; i < LED_STRIP_LENGTH * 20; i++) {
-            strip_4.setPixelColor(i, strip_4.Color(led_buffer[i].r, led_buffer[i].g, led_buffer[i].b));
-        }
-        for (int i = LED_STRIP_LENGTH * 20; i < LED_STRIP_LENGTH * 25; i++) {
-            strip_5.setPixelColor(i, strip_5.Color(led_buffer[i].r, led_buffer[i].g, led_buffer[i].b));
-        }
-        for (int i = LED_STRIP_LENGTH * 25; i < LED_STRIP_LENGTH * 30; i++) {
-            strip_6.setPixelColor(i, strip_6.Color(led_buffer[i].r, led_buffer[i].g, led_buffer[i].b));
-        }
-        for (int i = LED_STRIP_LENGTH * 30; i < LED_STRIP_LENGTH * 34; i++) {
-            strip_7.setPixelColor(i, strip_7.Color(led_buffer[i].r, led_buffer[i].g, led_buffer[i].b));
-        }
-        for (int i = LED_STRIP_LENGTH * 34; i < LED_STRIP_LENGTH * 38; i++) {
-            strip_8.setPixelColor(i, strip_8.Color(led_buffer[i].r, led_buffer[i].g, led_buffer[i].b));
-        }
+//        for (int i = LED_STRIP_LENGTH * 5; i < LED_STRIP_LENGTH * 10; i++) {
+//            strip_2.setPixelColor(i, strip_2.Color(led_buffer[i].r, led_buffer[i].g, led_buffer[i].b));
+//        }
+//        for (int i = LED_STRIP_LENGTH * 10; i < LED_STRIP_LENGTH * 15; i++) {
+//            strip_3.setPixelColor(i, strip_3.Color(led_buffer[i].r, led_buffer[i].g, led_buffer[i].b));
+//        }
+//        for (int i = LED_STRIP_LENGTH * 15; i < LED_STRIP_LENGTH * 20; i++) {
+//            strip_4.setPixelColor(i, strip_4.Color(led_buffer[i].r, led_buffer[i].g, led_buffer[i].b));
+//        }
+//        for (int i = LED_STRIP_LENGTH * 20; i < LED_STRIP_LENGTH * 25; i++) {
+//            strip_5.setPixelColor(i, strip_5.Color(led_buffer[i].r, led_buffer[i].g, led_buffer[i].b));
+//        }
+//        for (int i = LED_STRIP_LENGTH * 25; i < LED_STRIP_LENGTH * 30; i++) {
+//            strip_6.setPixelColor(i, strip_6.Color(led_buffer[i].r, led_buffer[i].g, led_buffer[i].b));
+//        }
+//        for (int i = LED_STRIP_LENGTH * 30; i < LED_STRIP_LENGTH * 34; i++) {
+//            strip_7.setPixelColor(i, strip_7.Color(led_buffer[i].r, led_buffer[i].g, led_buffer[i].b));
+//        }
+//        for (int i = LED_STRIP_LENGTH * 34; i < LED_STRIP_LENGTH * 38; i++) {
+//            strip_8.setPixelColor(i, strip_8.Color(led_buffer[i].r, led_buffer[i].g, led_buffer[i].b));
+//        }
         strip_1.show();
-        strip_2.show();
-        strip_3.show();
-        strip_4.show();
-        strip_5.show();
-        strip_6.show();
-        strip_7.show();
-        strip_8.show();
+//        strip_2.show();
+//        strip_3.show();
+//        strip_4.show();
+//        strip_5.show();
+//        strip_6.show();
+//        strip_7.show();
+//        strip_8.show();
         dma_transfer_finished = true;
         while (!usb_transfer_finished) {
             tight_loop_contents();
