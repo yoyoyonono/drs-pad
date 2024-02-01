5dbfa0901284851989d7cba5686144cf24d713ca
Aaron Thapa <notsmart98@gmail.com>
2024-02-01 14:11:09 +0545

    optimize byte array

--- py/main.py
+++ py/main.py
@@ -7,12 +7,6 @@ import websockets
 import serial
 from time import perf_counter
 
-@dataclasses.dataclass
-class LED:
-    r: int
-    g: int
-    b: int
-
 async def main():
     pygame.init()
     display = pygame.display.set_mode((38*10, 49*10))
@@ -29,12 +23,8 @@ async def main():
         rgb_values = [x & 0xFE for x in json.loads(message[:-1])["data"][0]]
         values = np.array(rgb_values, dtype=np.uint8).reshape(49, 38, 3)
         image = values.transpose(1, 0, 2)
-        leds = np.array([LED(*x) for x in values.reshape(49*38, 3)]).reshape(49, 38)
         t1_start = perf_counter()
-        message = bytes()
-        for x in range(38):
-            for y in range(49):
-                message += (bytes([leds[y, x].r, leds[y, x].g, leds[y, x].b]))                    
+        message = bytes(rgb_values)
         ser.write(message)
         t1_stop = perf_counter()
         print(f'{t1_stop - t1_start:.3f}s {1 / (t1_stop - t1_start):.3f}FPS {len(message)/1024/1024/(t1_stop - t1_start)*8:.3f}Mb/s')
