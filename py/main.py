fe52b70d2918bb3ed40f56ee9df66f1a4777e1ac
Aaron Thapa <notsmart98@gmail.com>
2024-02-29 14:56:25 +0545

    threads buggy

--- py/main.py
+++ py/main.py
@@ -6,45 +6,61 @@ import pygame
 import websockets
 import serial
 from time import perf_counter
+import threading
+import math
+
+response_data = [0] * 38 * 49 * 3
 
 reverse_bit = lambda x : int('{:08b}'.format(x)[::-1], 2)
 
-async def main():
+def clamp_led(x):
+    return math.floor(x * (255 / 215))
+
+async def socket_handler():
+    global response_data
+    async with websockets.connect("ws://localhost:9002") as socket:
+        print("connected")
+        while True:
+            try:
+                await socket.send(b'{"id":0,"module":"drs","function":"tapeled_get","params":[]}')
+                message = await socket.recv()
+                response_data = [clamp_led(x) & 0b1111_1110 for x in json.loads(message[:-1])["data"][0]]
+            except Exception:
+                continue
+
+def serial_send():
+    with serial.Serial('COM10', 460800) as ser:
+        while True:
+            old_time = perf_counter()
+            rgb_values = [reverse_bit(x) for x in response_data]
+            image = np.array(rgb_values, dtype=np.uint8).reshape(49, 38, 3).transpose(1, 0, 2)
+            uncompressed = bytes()
+            for x in range(38):
+                for y in range(49):
+                    if (x % 5) % 2 == 0:
+                        uncompressed += bytes([image[x, y, 1], image[x,y,0], image[x,y,2]])
+                    else:
+                        uncompressed += bytes([image[x, 48-y, 1], image[x,48-y,0], image[x,48-y,2]])
+            compressed = brotli.compress(bytes(uncompressed))
+            message = len(compressed).to_bytes(2, 'big') + compressed
+            print(ser.read(1))
+            ser.write(message)
+            new_time = perf_counter()
+            print(f'{new_time - old_time:.3f}s {1 / (new_time - old_time):.3f}FPS {len(message)/1024/1024/(new_time - old_time)*8:.3f}Mb/s {len(compressed)}B ', end='\t')
+
+def display():
     pygame.init()
-    clock = pygame.time.Clock()
     display = pygame.display.set_mode((38*10, 49*10))
-    ser = serial.Serial("COM10", 460800)
-    socket = await websockets.connect("ws://localhost:9002")
-    print("connected")
-    old_time = perf_counter()
+    clock = pygame.time.Clock()
     while True:
-        try:
-            await socket.send(b'{"id":0,"module":"drs","function":"tapeled_get","params":[]}')
-            message = await socket.recv()
-        except Exception:
-            socket = await websockets.connect("ws://localhost:9002")
-            continue
-        rgb_values = [reverse_bit(x & 0b1111_0000) for x in json.loads(message[:-1])["data"][0]]
-        image = np.array(rgb_values, dtype=np.uint8).reshape(49, 38, 3).transpose(1, 0, 2)
-        image_values = np.array([x & 0b1111_0000 for x in json.loads(message[:-1])["data"][0]]).reshape(49, 38, 3).transpose(1, 0, 2)
-        uncompressed = bytes()
-        for x in range(38):
-            for y in range(49):
-                if (x % 5) % 2 == 0:
-                    uncompressed += bytes([image[x, y, 1], image[x,y,0], image[x,y,2]])
-                else:
-                    uncompressed += bytes([image[x, 48-y, 1], image[x,48-y,0], image[x,48-y,2]])
-        compressed = brotli.compress(bytes(uncompressed))
-        message = len(compressed).to_bytes(2, 'big') + compressed
-        print(ser.read(1))
-        ser.write(message)
-        new_time = perf_counter()
-        print(f'{new_time - old_time:.3f}s {1 / (new_time - old_time):.3f}FPS {len(message)/1024/1024/(new_time - old_time)*8:.3f}Mb/s {len(compressed)}B ', end='\t')
-        old_time = perf_counter()
+        image_values = np.array(response_data).reshape(49, 38, 3).transpose(1, 0, 2)
         surface = pygame.surfarray.make_surface(image_values)
         display.blit(pygame.transform.scale(surface, (38*10, 49*10)), (0, 0))
         pygame.display.update()
         pygame.event.pump()
+        clock.tick(60)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    threading.Thread(target=display).start()
+    threading.Thread(target=serial_send).start()
+    asyncio.get_event_loop().run_until_complete(socket_handler())
